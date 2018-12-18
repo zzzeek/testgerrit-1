c9ff63e4be384668ad798fd8cc0c359ffd3f0424
Mike Bayer <mike_mp@zzzcomputing.com>
2018-12-18 11:52:52 -0500

    pr134
    change some things.
    
    this is the second rev on the github, which is out of
    sync w/ the gerrit.

--- foo.py
+++ foo.py
@@ -3,26 +3,27 @@
 from foo import bar
 from foo import bat
 
-import io  # noqa
 import re  # noqa
 
 
-def foo_one(x, y):
+def foo_one(x, y, z):
     """do foo one"""
     bar.do_x(x)
     bar.do_y(y)
+    bat.do_z(z)
 
 
 def foo_two(z, x, y):
     """do foo two"""
     bat.do_z(z * 12)
     bar.do_x(x).y(y).z(z)
-    bar.do_y(y)
+    bar.do_y(y + 9)
 
 
 def foo_three(a, g, j):
     """do foo three"""
     bat.do_a(a)
+    bat.do_g(g)
     bat.do_j(j)
 
 
