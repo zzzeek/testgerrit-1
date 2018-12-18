2be69c065aaa276763c0f9dfe8f56de7604712a8
Mike Bayer <mike_mp@zzzcomputing.com>
2018-12-17 21:16:04 -0500

    pr131
    rev1
    
    Change-Id: Iedcf29bd94d9dbbb0d2aae877ace55d9e340b2f4

--- bar.py
+++ bar.py
@@ -3,13 +3,21 @@
 from bar import bar
 from bar import bat
 
+name = None
+
 
 def bar_one(x, y):
     bar.do_x(x)
-    bar.do_y(y)
+    bar.do_y(y + 7)
 
 
 def bar_two(z, q):
+    bat.do_z(z)
+    if name:
+        bat.do_q(q)
+
+
+def bar_two_five(z, q):
     bat.do_z(z)
     bat.do_q(q)
 
